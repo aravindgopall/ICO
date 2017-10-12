1f10b90e9b7f5d4ad07437debd93982188c5ae0f
merlox <merloxdixcontrol@gmail.com>
2017-10-12 13:36:01 +0200

    Configured migrations to deploy the token, then the presale, then the crowdsale

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,38 @@
-var ConvertLib = artifacts.require("./ConvertLib.sol");
-var MetaCoin = artifacts.require("./MetaCoin.sol");
+const Drops = artifacts.require("./Drops.sol");
+const Presale = artifacts.require('./Presale.sol');
+const Crowdsale = artifacts.require('./Crowdsale.sol');
 
-module.exports = function(deployer) {
-  deployer.deploy(ConvertLib);
-  deployer.link(ConvertLib, MetaCoin);
-  deployer.deploy(MetaCoin);
-};
+module.exports = function(deployer, network) {
+   let token
+
+   if(network != 'live') {
+     console.log('Deploying contracts...')
+
+     // Deploy the token
+     deployer.then(() => {
+        return Drops.new()
+     }).then(tokenInstance => {
+        token = tokenInstance.address
+
+        return deployer.deploy(
+           Presale,
+           web3.eth.accounts[0],
+           token,
+           0,
+           0, {
+              from: web3.eth.accounts[0]
+           }
+        )
+     }).then(() => {
+        return deployer.deploy(
+           Crowdsale,
+           web3.eth.accounts[0],
+           token,
+           0,
+           0, {
+              from: web3.eth.accounts[0]
+           }
+        )
+     })
+  }
+}
