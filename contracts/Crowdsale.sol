880f1c1aefd62238b0f56d809ed0890883e847a7
merlox <merloxdixcontrol@gmail.com>
2017-10-12 13:37:56 +0200

    Created WIP presale and crowdsale for testing

---
+++ contracts/Crowdsale.sol
@@ -0,0 +1,7 @@
+pragma solidity 0.4.15;
+
+/// @title The crowdsale contract that will be used to sell the ICO tokens
+/// @author Merunas Grincalaitis <merunasgrincalaitis@gmail.com>
+contract Crowdsale {
+   // Work in progress
+}
